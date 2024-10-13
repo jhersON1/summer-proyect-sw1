64f7803e270bc5dc3b31c544135a46be1f2bad61
CesarKnight <cesarcaballero111@gmail.com>
2024-10-13 16:37:10 -0400

    layout mao meno hecho

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,12 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LayoutPageComponent } from './layout-page/layout-page.component';
+import { TopbarComponent } from './components/topbar/topbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ButtonModule } from 'primeng/button';
+
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LayoutPageComponent,
+    TopbarComponent,
+    FooterComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    BrowserModule,
+    RouterModule,
+
+    ButtonModule,
+  ],
+  exports: [
+    LayoutPageComponent
   ]
 })
 export class LayoutModule { }
