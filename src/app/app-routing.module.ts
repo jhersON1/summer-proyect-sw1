1cb071c2c5d437e45ec23a26898482bb0fe53b86
jherson <rodriguezpaul98@gmail.com>
2025-02-05 12:04:52 -0400

    change app rouing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -28,10 +28,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {
-    scrollPositionRestoration: 'enabled',
-    onSameUrlNavigation: 'reload'
-  })],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
