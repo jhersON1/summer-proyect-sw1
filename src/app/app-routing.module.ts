591107bf89ca5960a7f2488ceabe39139c5ae34a
jherson <rodriguezpaul98@gmail.com>
2025-02-05 10:58:07 -0400

    chance app route module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -28,7 +28,10 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    scrollPositionRestoration: 'enabled',
+    onSameUrlNavigation: 'reload'
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl :'https://backend-sw1.onrender.com',
-  wsUrl: 'https://backend-sw1.onrender.com'
-};
